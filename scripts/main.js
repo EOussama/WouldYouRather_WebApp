61d4863bd38e899c902538a65fdeb3606792eeb2
EOussama <oussama_gtacj@live.fr>
2018-04-30 13:12:45 +0100

    Design refactoration

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+const choices = [
+	{
+		choice1: "Have a cat",
+		choice2: "Have a dog",
+		count1: 0,
+		count2: 0
+	},
+	{
+		choice1: "Learn C#",
+		choice2: "Learn Java",
+		count1: 0,
+		count2: 0
+	},
+	{
+		choice1: "Be a mobile developer",
+		choice2: "Be a web developer",
+		count1: 0,
+		count2: 0
+	},
+	{
+		choice1: "Front-end",
+		choice2: "Back-end",
+		count1: 0,
+		count2: 0
+	},
+	{
+		choice1: "Cut off your left hand",
+		choice2: "Cur off your right leg",
+		count1: 0,
+		count2: 0
+	},
+	{
+		choice1: "Be deaf",
+		choice2: "Be blind",
+		count1: 0,
+		count2: 0
+	}
+	,
+	{
+		choice1: "Be rich but have a boring life",
+		choice2: "Be poor but have an exciting life",
+		count1: 0,
+		count2: 0
+	}
+];
